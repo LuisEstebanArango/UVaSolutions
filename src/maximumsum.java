e074507b7df45c618ba045e8499ffd91298ffda4
LuisEstebanArango <estebanarango14@hotmail.com>
2015-03-28 00:59:44 -0500

    28/03/2015

--- src/maximumsum.java
+++ src/maximumsum.java
@@ -1,9 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class maximumsum {
+
+public class maximumsum{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello Softwareiii");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String palabra = br.readLine();
+			String palabraEncriptada = "";
+			for (int i = 0; i < palabra.length(); i++) {
+				palabraEncriptada+=((char)(palabra.charAt(i)+2));
+			}
+			System.out.println(palabraEncriptada);
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	private static int getAleatorio() {
+		while(true){
+			int temp = (int) (Math.random()*100);
+			if(temp<51)
+				return temp;
+		}
+	}
 }
